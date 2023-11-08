fbfa445117b80fcbd37932354d1705368731d5c9
DorLex <dorozhinskiysanya@gmail.com>
2023-11-08 16:15:29 +0300

    refact grid service

--- generate_langton_ant_image.py
+++ generate_langton_ant_image.py
@@ -2,32 +2,16 @@ import numpy as np
 
 from project.ant import Ant
 from project.configs import WIDTH, HEIGHT, ANT_X_START, ANT_Y_START, Direction, Color
-from project.services.ant_service import move_ant, turn_clockwise, turn_counterclockwise
-from project.services.grid_service import change_color_to_black, change_color_to_white
+
+from project.services.grid_service import ant_change_grid
 from project.services.image_service import generate_png_from_array
 
 
 def main():
     grid = np.full((WIDTH, HEIGHT), Color.WHITE.value, dtype=np.uint8)
-
     ant = Ant(ANT_X_START, ANT_Y_START, Direction.UP.value)
 
-    black_cell_count = 0
-    while 0 <= ant.x < WIDTH and 0 <= ant.y < HEIGHT:
-
-        # Если находимся на белой клетке
-        if grid[ant.y, ant.x] == Color.WHITE.value:
-            turn_clockwise(ant)
-            change_color_to_black(grid, ant)
-            black_cell_count += 1
-
-        # Если находимся на черной клетке
-        else:
-            turn_counterclockwise(ant)
-            change_color_to_white(grid, ant)
-            black_cell_count -= 1
-
-        move_ant(ant)
+    grid, black_cell_count = ant_change_grid(grid, ant)
 
     generate_png_from_array(grid)
 
--- project/services/grid_service.py
+++ project/services/grid_service.py
@@ -1,4 +1,26 @@
-from project.configs import Color
+from project.configs import Color, WIDTH, HEIGHT
+from project.services.ant_service import turn_clockwise, turn_counterclockwise, move_ant
+
+
+def ant_change_grid(grid, ant):
+    black_cell_count = 0
+    while 0 <= ant.x < WIDTH and 0 <= ant.y < HEIGHT:
+
+        # Если находимся на белой клетке
+        if grid[ant.y, ant.x] == Color.WHITE.value:
+            turn_clockwise(ant)
+            change_color_to_black(grid, ant)
+            black_cell_count += 1
+
+        # Если находимся на черной клетке
+        else:
+            turn_counterclockwise(ant)
+            change_color_to_white(grid, ant)
+            black_cell_count -= 1
+
+        move_ant(ant)
+
+    return grid, black_cell_count
 
 
 def change_color_to_black(grid, ant):
